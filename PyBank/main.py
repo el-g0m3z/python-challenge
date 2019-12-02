071d213f65d5c14682da420deaa46233cf0ba5d1
el-g0m3z <thomas.a.gomez@gmail.com>
2019-12-02 13:38:42 -0600

    finished PyBank HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,37 +1,35 @@
 import os
 import csv
-import pprint
-import numpy
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath, newline="") as csvfile:
     csv_header = next(csvfile)
     row_count = sum(1 for row in csvfile)
+
 with open(csvpath, newline="") as csvfile2:
     csv_header = next(csvfile2)
     profit_losses = sum(int(r[1]) for r in csv.reader(csvfile2))
     average_changes = int(profit_losses / row_count)
 
-min_max = np.genfromtxt(csvpath, delimiter=",", skip_header=True)
-    print ("min value element : ", min_max.min(axis=0)[1])
-    print ("max value element : ", min_max.max(axis=0)[2])
+with open(csvpath, "r") as csvfile3:
+    csv_reader = csv.DictReader(csvfile3)
+    data = list(csv_reader)
+    maxProfit = max(data, key=lambda x: x["Profit/Losses"])
+    minProfit = min(data, key=lambda x: x["Profit/Losses"])
 
-    print("Financial Analysis", file=open("output.txt", "a"))
+    print("Financial Analysis")
+    print("--------------------------")
+    print("Total Months:", row_count)
+    print("Total: $", profit_losses)
+    print("Average Change: $", average_changes)
+    print("Greatest Increase in Profits:", maxProfit)
+    print("Greatest Decrease in Profits:", minProfit)
+
+    print("Financial Analysis", file=open("output.txt", "w"))
+    print("--------------------------", file=open("output.txt", "a"))
     print("Total Months:", row_count, file=open("output.txt", "a"))
     print("Total: $", profit_losses, file=open("output.txt", "a"))
     print("Average Change: $", average_changes, file=open("output.txt", "a"))
-    print("Greatest Increase in Profits:", file=open("output.txt", "a"))
-    print("Greatest Decrease in Profits:", file=open("output.txt", "a"))
-
-results_file = open
-# csvreader = csv.reader(csvfile, delimiter=",")
-
-# print(csvreader)
-
-
-# print(f"CSV Header: {csv_header}")
-
-# for row in csvreader:
-
-#     print(row)
+    print("Greatest Increase in Profits:", maxProfit, file=open("output.txt", "a"))
+    print("Greatest Decrease in Profits:", minProfit, file=open("output.txt", "a"))
